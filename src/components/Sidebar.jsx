deff1cdf0b49881476658c4c11fe114b8473ac5a
olawaleraji <savagedotjsx@gmail.com>
2024-08-28 17:48:32 +0100

    yea

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -138,7 +138,7 @@ const Sidebar = () => {
                 <span className='sr-only'>Notifications</span>
               </button>
 
-              <button className='bg-blue-800 text-white px-4 py-2 rounded-md'>
+              <button className='bg-blue-800 text-white sm:px-4 sm:py-2 rounded-md text-[6px] sm:text-sm px-2'>
                 Create appointment
               </button>
             </div>
