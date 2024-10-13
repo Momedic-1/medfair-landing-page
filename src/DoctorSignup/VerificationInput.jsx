94e3cbfa53505ae989aefb073391858b31a4763b
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 <ned@workmail.com>
2024-10-14 00:59:59 +0100

    added new routes to make the page work

--- src/DoctorSignup/VerificationInput.jsx
+++ src/DoctorSignup/VerificationInput.jsx
@@ -1,35 +1,33 @@
 import  {useRef, useState,useEffect} from 'react'
 import {baseUrl} from "../env.jsx";
-
-const VerificationInput = ({setCurrentSte}) => {
-  const [code, setCode] = useState(['', '', '', '', ''])
-  const [userName, setUserName] = useState('')
-  const [userEmail, setUserEmail] = useState('')
+import {useNavigate} from "react-router-dom";
+const VerificationInput = () => {
+    const navigate = useNavigate();
+    const [code, setCode] = useState(['', '', '', '', ''])
+    const [userName, setUserName] = useState('')
+    const [userEmail, setUserEmail] = useState('')
     const inputRefs = useRef([]);
     const [loading, setLoading] = useState(false);
     const [verificationToken, setVerificationToken] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
 
-  // Load user data from localStorage
-  useEffect(() => {
-    const userData = JSON.parse(localStorage.getItem('userData'))
-    const email = JSON.parse(localStorage.getItem('email'))
-    if (userData) {
-      setUserName(userData.name || 'User')  // Set the name from userData, default to 'User' if not available
-      setUserEmail(email)    // Set the email from userData
-    }
-  }, [])
+    // Load user data from localStorage
+    useEffect(() => {
+        const userData = JSON.parse(localStorage.getItem('userData'))
+        const email = JSON.parse(localStorage.getItem('email'))
+        if (userData) {
+            setUserName(userData.name || 'User')  // Set the name from userData, default to 'User' if not available
+            setUserEmail(email)    // Set the email from userData
+        }
+    }, [])
 
     async function verifyEmail(){
-        console.log("calling verifyEmail")
-
         setLoading(true);
         const verificationData = { token: verificationToken, email: userEmail}
         console.log(verificationData)
         try {
             const response = await fetch(`https://momedic.onrender.com/api/v1/registration/verify-email`, {
-            // const response = await fetch(`http://localhost:8081/api/v1/registration/verify-email`, {
-            //     const response = await fetch(`${baseUrl}/api/v1/registration/verify-email`, {
+                // const response = await fetch(`${baseUrl}/api/v1/registration/verify-email`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -45,8 +43,9 @@ const VerificationInput = ({setCurrentSte}) => {
                 result = await response.text();
             }
             if (result.includes('Email verification successful')) {
+                navigate('/verification-success');
                 setLoading(false)
-                // setCurrentStep(2);
+                // setCurrentStep(3);
             } else {
                 setLoading(false);
                 setErrorMessage('Incorrect token! Please try again.');
@@ -59,64 +58,64 @@ const VerificationInput = ({setCurrentSte}) => {
         }
     }
 
-  const handleChange = (index, value) => {
-    const newCode = [...code]
-    newCode[index] = value
-    setCode(newCode)
+    const handleChange = (index, value) => {
+        const newCode = [...code]
+        newCode[index] = value
+        setCode(newCode)
 
-    if (value && index < 4) {
-      document.getElementById(`input-${index + 1}`).focus()
+        if (value && index < 4) {
+            document.getElementById(`input-${index + 1}`).focus()
+        }
+        if (index === 4) {
+            // When the last input is filled, set the verification token
+            setVerificationToken(newCode.join(''));
+            console.log(newCode.join(''))
+        } else if (value && index < 4) {
+            inputRefs.current[index + 1].focus();
+        }
+
     }
-      if (index === 4) {
-          // When the last input is filled, set the verification token
-          setVerificationToken(newCode.join(''));
-          console.log(newCode.join(''))
-      } else if (value && index < 4) {
-          inputRefs.current[index + 1].focus();
-      }
 
-  }
+    return (
 
-  return (
-    
-    <div className='bg-white mr-10 rounded-lg p-6'>
-  <h2 className='text-xl font-bold text-center mb-4 text-[#020E7C]'>Check your email, {userName}!</h2>
-  <p className='text-sm text-center text-gray-400 font-medium mb-4'>
-    A verification code was sent to {userEmail}.
-  </p>
-  <p className='text-sm text-center mb-4 font-medium text-gray-400'>
-    Enter the 5-digit code to verify your Medfair account
-  </p>
-  <p className='text-sm text-center text-blue-500 mb-4'>Open email app</p>
+        <div className='bg-white mr-10 rounded-lg p-6'>
+            <h2 className='text-xl font-bold text-center mb-4 text-[#020E7C]'>Check your email, {userName}!</h2>
+            <p className='text-sm text-center text-gray-400 font-medium mb-4'>
+                A verification code was sent to {userEmail}.
+            </p>
+            <p className='text-sm text-center mb-4 font-medium text-gray-400'>
+                Enter the 5-digit code to verify your Medfair account
+            </p>
+            <p className='text-sm text-center text-blue-500 mb-4'>Open email app</p>
 
- 
-  <div className='flex flex-col items-center space-y-4 mb-16'>
-    <div className='flex space-x-2'>
-      {code.map((digit, index) => (
-        <input
-          key={index}
-          id={`input-${index}`}
-          type='text'
-          maxLength='1'
-          className='w-12 h-12 text-center border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
-          value={digit}
-          onChange={e => handleChange(index, e.target.value)}
-        />
-      ))}
-    </div>
-    
-    
-    <button
-      onClick={() => verifyEmail()}
-      className={`md:w-[30%] bg-[#020E7C] text-white py-2 rounded-md hover:bg-blue-800 transition duration-300 w-[300px] lg:w-[55%]  px-3 inline-flex items-center justify-center gap-x-1 text-sm font-semibold  border border-transparent disabled:opacity-50 disabled:pointer-events-none`}
-    >
-      Next
-    </button>
-  </div>
-</div>
 
-    
-  )
+            <div className='flex flex-col items-center space-y-4 mb-16'>
+                <div className='flex space-x-2'>
+                    {code.map((digit, index) => (
+                        <input
+                            key={index}
+                            id={`input-${index}`}
+                            type='text'
+                            maxLength='1'
+                            className='w-12 h-12 text-center border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+                            value={digit}
+                            onChange={e => handleChange(index, e.target.value)}
+                        />
+                    ))}
+                </div>
+
+
+                <button
+                    onClick={() => verifyEmail()}
+                    className={`md:w-[30%] bg-[#020E7C] text-white py-2 rounded-md hover:bg-blue-800 transition duration-300 w-[300px] lg:w-[55%]  px-3 inline-flex items-center justify-center gap-x-1 text-sm font-semibold  border border-transparent disabled:opacity-50 disabled:pointer-events-none`}
+                >
+                    Next
+                </button>
+            </div>
+        </div>
+
+
+    )
 }
 
 export default VerificationInput
