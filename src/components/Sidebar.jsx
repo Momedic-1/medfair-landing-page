b746b830449096ccb85fb549848a4eee364d8b26
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 <ned@workmail.com>
2024-10-28 06:57:36 +0100

    refactored

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { NavLink } from 'react-router-dom'
+import MagnifyingGlass from "../assets/MagnifyingGlass.jsx";
+import NotificationBell from "../assets/NotificationBell.jsx";
 const Sidebar = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
   const sidebarRef = useRef(null)
@@ -31,71 +33,12 @@ const Sidebar = () => {
       <header className='sticky top-0 inset-x-0 px-4 flex flex-wrap md:justify-start md:flex-nowrap z-[48] w-full bg-white border-b text-sm py-2.5 lg:ps-[260px] overflow-x-hidden'>
           <nav className='px-4 sm:px-6 flex basis-full items-center w-full mx-auto flex-col'>
 
-    <div className='flex items-center px-3 justify-between w-full mb-2 lg:hidden'>
-      {/* Hamburger Menu */}
-      <button
-        type='button'
-        className='flex justify-center items-center gap-x-2 border border-gray-200 text-gray-800 hover:text-gray-500 rounded-lg focus:outline-none focus:text-gray-500 disabled:opacity-50 disabled:pointer-events-none'
-        onClick={toggleSidebar}
-        aria-label='Toggle Navigation'
-      >
-        <span className='sr-only'>Toggle Navigation</span>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          strokeWidth='1.5'
-          stroke='currentColor'
-          className='size-6'
-        >
-          <path strokeLinecap='round' strokeLinejoin='round' d='M3.75 9h16.5m-16.5 6.75h16.5' />
-        </svg>
-      </button>
-
-      {/* Notification */}
-      <button
-        type='button'
-        className='size-[39px] relative inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none'
-      >
-        <svg
-          className='shrink-0 size-4'
-          xmlns='http://www.w3.org/2000/svg'
-          width='24'
-          height='24'
-          viewBox='0 0 24 24'
-          fill='none'
-          stroke='currentColor'
-          strokeWidth='2'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        >
-          <path d='M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9' />
-          <path d='M10.3 21a1.94 1.94 0 0 0 3.4 0' />
-        </svg>
-        <span className='sr-only'>Notifications</span>
-      </button>
-    </div>
-
     {/* Mobile Layout: Second Row for Search and Create Appointment */}
     <div className='flex items-center px-4 justify-between w-full lg:hidden'>
       {/* Search Bar */}
       <div className='relative flex-grow w-[7rem] mr-2'>
         <div className='absolute inset-y-0 left-0 flex items-center pointer-events-none ps-3.5'>
-          <svg
-            className='shrink-0 size-4 text-gray-400'
-            xmlns='http://www.w3.org/2000/svg'
-            width='24'
-            height='24'
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='currentColor'
-            strokeWidth='2'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-          >
-            <circle cx='11' cy='11' r='8' />
-            <path d='m21 21-4.3-4.3' />
-          </svg>
+          <MagnifyingGlass/>
         </div>
         <input
           type='text'
@@ -114,21 +57,7 @@ const Sidebar = () => {
     <div className='hidden md:flex md:flex-row items-center justify-between gap-1 md:gap-x-3 w-full'>
       <div className='relative w-[40rem]'>
         <div className='absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-3.5'>
-          <svg
-            className='shrink-0 size-4 text-gray-400'
-            xmlns='http://www.w3.org/2000/svg'
-            width='24'
-            height='24'
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='currentColor'
-            strokeWidth='2'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-          >
-            <circle cx='11' cy='11' r='8' />
-            <path d='m21 21-4.3-4.3' />
-          </svg>
+          <MagnifyingGlass/>
         </div>
         <input
           type='text'
@@ -146,21 +75,7 @@ const Sidebar = () => {
         type='button'
         className='size-[38px] relative inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none'
       >
-        <svg
-          className='shrink-0 size-4'
-          xmlns='http://www.w3.org/2000/svg'
-          width='24'
-          height='24'
-          viewBox='0 0 24 24'
-          fill='none'
-          stroke='currentColor'
-          strokeWidth='2'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        >
-          <path d='M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9' />
-          <path d='M10.3 21a1.94 1.94 0 0 0 3.4 0' />
-        </svg>
+        <NotificationBell/>
         <span className='sr-only'>Notifications</span>
       </button>
       <button className='bg-blue-800 text-white py-2 font-bold sm:px-4 sm:py-4 rounded-md sm:text-sm px-2'>
