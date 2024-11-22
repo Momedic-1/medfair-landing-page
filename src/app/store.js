85a7a98f9f87c941bc13cd5daa10bc23cdc37296
Favourmbata <134517996+Favourmbata@users.noreply.github.com>
2024-11-22 12:11:54 +0100

    removed comment

--- src/app/store.js
+++ src/app/store.js
@@ -1,39 +1,4 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import counterReducer from '../features/counter/counterSlice'
-// import { setupListeners } from '@reduxjs/toolkit/query';
-// import { registrationApi } from "../redux-query/RegistrationQuery";
 
-// export const store = configureStore({
-//     reducer: {
-//         counter: counterReducer,
-//         [registrationApi.reducerPath]: registrationApi.reducer,
-//     },
-//     middleware: (getDefaultMiddleware) =>
-//         getDefaultMiddleware()
-//               .concat(registrationApi.middleware)
-//   })
-//   setupListeners(store.dispatch)
-  
-// import { configureStore } from '@reduxjs/toolkit';
-// import authReducer from '../redux-query/authSlice';
-// import { persistReducer,persistStore } from 'redux-persist';
-// import storage from "redux-persist/lib/storage"
-
-
-// const persistConfig = {
-//     key: 'root',
-//     storage,
-//   };
-//   const persistedReducer = persistReducer(persistConfig, authReducer);
-
-//   export const store = configureStore({
-//     reducer: {
-//       auth: persistedReducer,
-//     },
-//   });
-  
-// export const persistor = persistStore(store);
-// store.js
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/authSlice';
 
