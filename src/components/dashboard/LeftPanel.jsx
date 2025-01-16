c0245ff29b907de2cf95714badb4e1038589787b
Hunchojoe19 <enubiakjoseph@gmail.com>
2025-01-16 14:53:35 +0100

    changed th edirection of the calendar

--- src/components/dashboard/LeftPanel.jsx
+++ src/components/dashboard/LeftPanel.jsx
@@ -11,12 +11,12 @@ function LeftPanel({status}) {
   return (
     <div className='w-full py-4'>
       <WelcomeBack status={status} />
-      <div className='w-full flex flex-col lg:flex-row lg:justify-between lg:items-start justify-center px-2 lg:px-0 items-center mt-4 gap-x-4 lg:gap-x-8'>
+      <div className='w-full flex flex-col-reverse gap-y-6 lg:flex-row lg:justify-between lg:items-start justify-center px-2 lg:px-0 items-center mt-4 gap-x-4 lg:gap-x-8'>
         <div className="flex justify-between items-start w-full lg:w-[40%]">
 
       <AppointmentRequests appointments={appointments}/>
       </div>
-      <div className='w-full lg:w-[60%] mt-6 lg:mt-0'>
+      <div className='w-full lg:w-[60%] lg:mt-0'>
 
         <Appointments />
       </div>
