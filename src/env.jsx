7b7b7cf8655d5784027fcde08f4f2b56d9ef6e5c
JohnNkwachi <108151968+johnNkwachi@users.noreply.github.com>
2025-10-01 11:35:07 +0100

    Update env.jsx
    Signed-off-by: JohnNkwachi <108151968+johnNkwachi@users.noreply.github.com>

--- src/env.jsx
+++ src/env.jsx
@@ -1,5 +1,5 @@
 // export const baseUrl = import.meta.env.VITE_API_URL
-   export const baseUrl = "https://momedic.onrender.com"
+   export const baseUrl = "https://backend-h3k6.onrender.com"
 // export const baseUrl = "https://backend-aywq.onrender.com"
 // export const baseUrl = "https://momedic.onrender.com"
 // export const baseUrl = "http://localhost:8081"
